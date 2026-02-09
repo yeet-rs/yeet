bb57c68407da8550165f522966bebfab66e1a0c0
Srylax <71783705+Srylax@users.noreply.github.com>
2026-02-09 07:41:21 +0100

    nix: yeetd add zlink hash

--- packages/yeetd.nix
+++ packages/yeetd.nix
@@ -5,7 +5,12 @@ in
 pkgs.rustPlatform.buildRustPackage {
   pname = manifest.name;
   version = manifest.version;
-  cargoLock.lockFile = ../Cargo.lock;
+  cargoLock = {
+    lockFile = ../Cargo.lock;
+    outputHashes = {
+      "zlink-0.4.0" = "sha256-cS8Oi9zaDcpnP9v12pNSuDEK25KkyC1x55YMcg27qQI=";
+    };
+  };
   src = ../.;
   buildAndTestSubdir = "yeet-server";
   nativeBuildInputs = with pkgs; [
