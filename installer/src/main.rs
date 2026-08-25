a65649baf624fe1e722d8ce9008fb5cfc1a23269
Srylax <71783705+Srylax@users.noreply.github.com>
2026-08-25 21:38:30 +0200

    installer: disk anchor map

--- installer/src/main.rs
+++ installer/src/main.rs
@@ -1,6 +1,9 @@
-use std::{collections::HashMap, fs};
+use std::{collections::HashMap, fs, hash::Hash};
 
-use color_eyre::Result;
+use color_eyre::{
+    Result,
+    eyre::{Ok, OptionExt},
+};
 
 fn main() -> Result<()> {
     // 1. Get a list of disko configurations
@@ -19,12 +22,40 @@ fn main() -> Result<()> {
     Ok(())
 }
 
+fn map_disko_anchors(
+    anchors: Vec<String>,
+    mut disks: Vec<String>,
+) -> Result<HashMap<String, String>> {
+    let mut map = HashMap::new();
+    for anchor in anchors {
+        let disk = inquire::Select::new(&format!("Select the disk for `{anchor}`"), disks.clone())
+            .prompt()?;
+        map.insert(anchor, disk.clone());
+        disks.retain(|x| *x != disk);
+    }
+    Ok(map)
+}
+
 /// replaces every `INSTALLER_DISK` with the corresponding disk
 fn replace_disko_devices(mut disko: String, map: HashMap<String, String>) -> String {
     for (anchor, disk) in map {
-        disko = disko.replace(&anchor, &disk);
+        disko = disko.replace(&format!("INSTALLER_DISK_{anchor}"), &disk);
     }
     disko
+}
+
+/// returns all anchors marked with `INSTALLER_DISK`
+fn get_disko_anchors(disko: &str) -> Result<Vec<String>> {
+    let mut anchors = Vec::new();
+    let mut remainder = disko;
+    while let Some((_rest, anchor)) = remainder.split_once("INSTALLER_DISK_") {
+        let anchor = anchor
+            .split_once('"')
+            .ok_or_eyre("Unexpected end of `INSTALLER_DISK`_ anchor")?;
+        remainder = anchor.1;
+        anchors.push(anchor.0.to_owned());
+    }
+    Ok(anchors)
 }
 
 fn list_devices() -> Result<Vec<String>> {
@@ -39,10 +70,7 @@ fn list_devices() -> Result<Vec<String>> {
         }
 
         // skip hidden gendisks
-        if fs::read_to_string(path.join("hidden"))
-            .map(|s| s.trim() == "1")
-            .unwrap_or(false)
-        {
+        if fs::read_to_string(path.join("hidden")).is_ok_and(|gendisk| gendisk.trim() == "1") {
             continue;
         }
 
@@ -55,12 +83,20 @@ fn list_devices() -> Result<Vec<String>> {
 mod test {
     use std::collections::HashMap;
 
-    use crate::replace_disko_devices;
+    use crate::{get_disko_anchors, replace_disko_devices};
 
     #[test]
-    fn test_replacement() {
-        let after=replace_disko_devices(r#"{disko.devices = {disk = {main = {device = "/dev/INSTALLER_DISK_main";};two = {device = "/dev/INSTALLER_DISK_two";};};};}"#.into(),
-            HashMap::from([("INSTALLER_DISK_main".into(),"sda".into()),("INSTALLER_DISK_two".into(),"sdb".into())]));
-        assert_eq!(after,r#"{disko.devices = {disk = {main = {device = "/dev/sda";};two = {device = "/dev/sdb";};};};}"#.to_string())
+    fn disko_anchor_replace() {
+        let after = replace_disko_devices(r#"{disko.devices = {disk = {main = {device = "/dev/INSTALLER_DISK_main";};two = {device = "/dev/INSTALLER_DISK_two";};};};}"#.into(),
+            HashMap::from([("main".into(),"sda".into()),("two".into(),"sdb".into())]));
+        assert_eq!(after,r#"{disko.devices = {disk = {main = {device = "/dev/sda";};two = {device = "/dev/sdb";};};};}"#.to_owned());
+    }
+
+    #[test]
+    fn disko_anchor_extract() {
+        let anchors = get_disko_anchors(
+            r#"{disko.devices = {disk = {main = {device = "/dev/INSTALLER_DISK_main";};two = {device = "/dev/INSTALLER_DISK_two";};};};}"#,
+        ).unwrap();
+        assert_eq!(anchors, vec!["main".to_owned(), "two".to_owned()]);
     }
 }
