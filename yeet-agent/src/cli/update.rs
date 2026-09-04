da7616895faddbda5d58c56b6372373d16034976
Srylax <71783705+srylax@users.noreply.github.com>
2026-09-04 09:28:32 +0200

    cli: fix `--yes` logic in yeet-udpate

--- yeet-agent/src/cli/update.rs
+++ yeet-agent/src/cli/update.rs
@@ -28,9 +28,6 @@ pub async fn update(args: UpdateArgs) -> Result<()> {
     .await?;
 
     match dry_run_result {
-        varlink::DownloadUpdateResult::Downloaded => {
-            info!("File is already downloaded");
-        }
         varlink::DownloadUpdateResult::UpToDate => {
             info!("You are Up To Date");
             return Ok(());
@@ -39,21 +36,18 @@ pub async fn update(args: UpdateArgs) -> Result<()> {
             info!("You are currently detached");
             return Ok(());
         }
-        varlink::DownloadUpdateResult::DryRun => {}
+        varlink::DownloadUpdateResult::DryRun | varlink::DownloadUpdateResult::Downloaded => {}
     }
 
-    if !args.yes
-        && matches!(dry_run_result, varlink::DownloadUpdateResult::DryRun)
-        && !inquire::Confirm::new("Do you want to download this update (without installing)?")
-            .with_default(true)
-            .prompt()?
-    {
-        // User does not want to update yet
-        return Ok(());
-    }
+    if !args.yes && matches!(dry_run_result, varlink::DownloadUpdateResult::DryRun) {
+        let download =
+            inquire::Confirm::new("Do you want to download this update (without installing)?")
+                .with_default(true)
+                .prompt()?;
 
-    // we do not need to run it a second time if it is already downloaded
-    if matches!(dry_run_result, varlink::DownloadUpdateResult::DryRun) {
+        if !download {
+            return Ok(());
+        }
         varlink::download_update(
             false,
             vec![
@@ -73,12 +67,12 @@ pub async fn update(args: UpdateArgs) -> Result<()> {
     )
     .await?;
     match dry_run_result {
-        varlink::ActivateUpdateResult::DryRun => {}
+        varlink::ActivateUpdateResult::DryRun | varlink::ActivateUpdateResult::Activated => {}
         varlink::ActivateUpdateResult::NotDownloaded => {
             bail!("Downloading the update was not succesfull")
         }
-        varlink::ActivateUpdateResult::Activated
-        | varlink::ActivateUpdateResult::AlreadySwitched => {
+
+        varlink::ActivateUpdateResult::AlreadySwitched => {
             info!("You are already running on the newest version");
             return Ok(());
         }
@@ -89,24 +83,24 @@ pub async fn update(args: UpdateArgs) -> Result<()> {
         }
     }
 
-    if !args.yes
-        && matches!(dry_run_result, varlink::ActivateUpdateResult::DryRun)
-        && !inquire::Confirm::new("Do you want to install this update (might cause a logout)?")
-            .with_default(true)
-            .prompt()?
-    {
-        // User does not want to update yet
-        return Ok(());
+    if !args.yes && matches!(dry_run_result, varlink::ActivateUpdateResult::DryRun) {
+        let switch =
+            inquire::Confirm::new("Do you want to install this update (might cause a logout)?")
+                .with_default(true)
+                .prompt()?;
+        if !switch {
+            return Ok(());
+        }
+        varlink::activate_update(
+            false,
+            vec![
+                nix::unistd::dup(io::stdout())?,
+                nix::unistd::dup(io::stderr())?,
+            ],
+        )
+        .await?;
     }
 
-    varlink::activate_update(
-        false,
-        vec![
-            nix::unistd::dup(io::stdout())?,
-            nix::unistd::dup(io::stderr())?,
-        ],
-    )
-    .await?;
     let version = crate::nix::nixos_version()?;
     info!(
         "Updated to {}: {}",
